a3fb7b79cbc410d91320833fb2df529043984224
Gaurav Vijay Dave <122374158+gauravdave083@users.noreply.github.com>
2024-06-29 14:22:54 +0000

    ddddd

--- Basics.py
+++ Basics.py
@@ -35,3 +35,11 @@ print(x)
 x = {"name" : "John", "age" : 36}
 print(type(x))
 print(x)
+
+#Use the len function to print the length of the string.
+x = "Hello World"
+print(len(x))
+
+#Get the first character of the string txt.
+txt = "Hello World"
+x = txt[0]
