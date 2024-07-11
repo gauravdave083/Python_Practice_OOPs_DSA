4c99dd4b41b3e0480636004557342c8ed22680a3
Gaurav Vijay Dave <122374158+gauravdave083@users.noreply.github.com>
2024-07-11 11:51:44 +0000

    dg

---
+++ ApnaCollegeSoln/Odd_of_even.py
@@ -0,0 +1,11 @@
+def OddOrEven(number):
+    if number % 2 == 0:
+            return "Even"
+    else: 
+          return "Odd"
+      
+#Take input for the problem
+number = int(input("Enter the number: "))
+      
+result = OddOrEven(number)
+print(f"The number is {result}.")
