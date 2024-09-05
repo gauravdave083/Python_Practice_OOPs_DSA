f327f7e0bf8245cefe755e02837f392df1f13421
gauravdave083 <gd836398@gmail.com>
2024-09-05 22:10:49 +0530

    OOPs

---
+++ OOPs/Encapsulation.py
@@ -0,0 +1,6 @@
+# ENCAPSULATION
+
+# It describes the idea of wrapping data and the methods that work on data within one unit. This puts restrictions on accessing variables and methods directly and can prevent the accidental modification of data.
+
+# PROBLEM: Modify the Car class to encapsulate the brand attribute, making it private, and provide a getter method for it.
+
