e48992e365d4e6947172352fd73481be0b7c5fd1
Gaurav Vijay Dave <122374158+gauravdave083@users.noreply.github.com>
2025-01-09 05:28:38 +0000

    practice

---
+++ Practice_Set/Chapter2.py
@@ -0,0 +1,43 @@
+# Write a python program to add two numbers
+
+a = 3
+b = 2
+print(a + b)
+
+
+
+
+# Write a python to find remainder when a number is divided by z.
+
+number = 3
+z = 4
+print(number%z)
+
+
+
+
+# Check Type
+
+a = input("Enter the value of a: ")
+print(type(a))
+
+
+
+
+# a is greater than b or not
+
+a = int(input("Enter A: "))
+b = int(input("Enter B: "))
+
+print("a is greater than b is", a>b)
+
+
+
+
+# Square root
+
+number = int(input("Enter the number: "))
+
+number = number*number
+
+print("Square root of the number is: ", number)
