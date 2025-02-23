d9cb23558a10a035314e0a0b9bf2d2e6bb69aef9
Gaurav Vijay Dave <122374158+gauravdave083@users.noreply.github.com>
2025-02-23 08:14:15 +0000

    practice

--- Top_100_Code/Three.py
+++ Top_100_Code/Three.py
@@ -29,7 +29,7 @@ while num > 0:
 print(reverse)
 
 
-# Palindrome Program in Python
+# 3. Palindrome Program in Python
 
 num = 1221
 temp = num
@@ -44,3 +44,30 @@ if num == reverse:
     print('Palindrome')
 else:
     print("Not a palindrome")
+
+
+# 4. Find the Nth Term of a Fibonacci Series in Python
+
+def Fibonacci(n):
+    if n < 2:
+        return n
+    return Fibonacci(n-1) + Fibonacci(n-2)
+
+n = 6
+print(Fibonacci(n-1))
+
+
+# 5. Whrite a program to print fibonacci series upto n terms in python
+
+number = 10
+n1, n2 = 0, 1
+
+print("Fibonacci Series:", n1, n2, end=" ")
+
+for i in range(2, number):
+    n3 = n2 + n1
+    n1 = n2
+    n2 = n3
+    print(n3, end=" ")
+
+print()
